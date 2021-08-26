c6f3aa8371c1e31e1b9b3e7f328090a5cf147678
Chris P <christopher.mark.persaud@gmail.com>
2021-08-26 14:54:31 -0400

    add chart updater

---
+++ update-chart.r
@@ -0,0 +1,36 @@
+source('download-process.r')
+# devtools::install_github("munichrocker/DatawRappr",ref="master")
+
+library(DatawRappr)
+
+print("Starting chart updater")
+
+updateDateFormat <- gsub(
+  pattern = " 0",
+  replacement = ' ',
+  x = format(
+    x = max(fl$date),
+    format = "%B %d, %Y"
+  )
+)
+
+chartIDs <- c(
+  'ULrmv' # new deaths reported
+)
+
+apikey <- Sys.getenv("DATAWRAPPER_API")
+
+for (id in chartIDs) {
+  print(id)
+  dw_edit_chart(
+    chart_id = id,
+    api_key = apikey,
+    annotate = paste0("As of ",updateDateFormat,'. New deaths are the result of calculating the difference between the latest death toll and previously reported tally from the Florida Department of Health or the U.S. Center for Disease Control and Prevention, whichever is higher.')
+  )
+  print("Publishing chart")  
+  dw_publish_chart(
+    chart_id = id,
+    api_key = apikey,
+    return_urls = TRUE
+  )  
+}
