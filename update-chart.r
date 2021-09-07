0ee566c4a41269879626bf3c57ab856799e8e279
Chris P <christopher.mark.persaud@gmail.com>
2021-09-07 16:14:40 -0400

    typo fix

--- update-chart.r
+++ update-chart.r
@@ -27,7 +27,7 @@ for (id in chartIDs) {
   dw_edit_chart(
     chart_id = id,
     api_key = apikey,
-    annotate = paste0("As of ",updateDateFormat,'. Daily estimates are calculted based on numbers reported weekly by Florida\'s health department and daily by the U.S. Centers for Disease Control and Prevention.')
+    annotate = paste0("As of ",updateDateFormat,'. Daily estimates are calculated based on numbers reported weekly by Florida\'s health department and daily by the U.S. Centers for Disease Control and Prevention.')
   )
   print("Publishing chart")  
   dw_publish_chart(
